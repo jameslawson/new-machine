0069727d3aaec8b16d6c18f3935d90ec89bb312e
James Lawson <jameslawson@users.noreply.github.com>
2019-05-23 14:16:04 +0100

    Add add_remove_proxy.sh and configure_npm.sh

---
+++ configure_npm.sh
@@ -0,0 +1,5 @@
+npm config set init.author.name $name
+npm config set init.author.email $email
+npm config set proxy $HTTP_PROXY
+npm config set http_proxy $HTTP_PROXY
+npm config set https-proxy $HTTP_PROXY
