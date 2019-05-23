a4ccd5410fa9775fcfed95985b40006c66b0d57d
James Lawson <jameslawson@users.noreply.github.com>
2019-05-23 14:23:47 +0100

    Add vimdiff to configure_git.sh

--- configure_git.sh
+++ configure_git.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 echo ">>>> Configuring git"
+
 git config --system http.proxy $HTTP_PROXY
 git config --system https.proxy $HTTPS_PROXY
 git config --global user.name "James Lawson"
 git config --global user.email "jameslawson@users.noreply.github.com"
+
+# -- use vimdiff
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+
 git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.ci commit -v
 git config --global alias.lg log --graph --oneline --decorate --all
+git config --global alias.d difftool
+
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 git config --global core.excludesfile ~/.gitignore_global
 git config --global core.hooksPath ~/.git-templates/hooks
+
 echo ">>> Success: configured git"
-echo ">>> You can view git config using: git config --list --show-origin"
+echo ">>> You can view all git config using: git config --list --show-origin"
 echo ">>> You can view global config using: git config --list --global"
 echo ">>> You can view system config using: git config --list --system"
 
